23d4303e6ede1e5fd69cc3e280a2739d58084d5a
PC Chin <chin.pengcheng@gmail.com>
2019-04-15 22:56:26 +0800

    Added back twice to exit

--- app/src/main/java/com/pcchin/studyassistant/MainActivity.java
+++ app/src/main/java/com/pcchin/studyassistant/MainActivity.java
@@ -1,13 +1,37 @@
 package com.pcchin.studyassistant;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean doubleBackToExitPressedOnce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onBackPressed() {
+        // Press back to exit
+        if (doubleBackToExitPressedOnce) {
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(0);
+        } else {
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show();
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 1500);
+        }
+    }
+
 }

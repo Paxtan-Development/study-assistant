8f42520a5c73581aa5ecd758238bc8ef31b064f4
PC Chin <chin.pengcheng@gmail.com>
2019-06-26 22:42:12 +0800

    Fixed migration error

--- app/src/main/java/com/pcchin/studyassistant/notes/database/NotesSubjectMigration.java
+++ app/src/main/java/com/pcchin/studyassistant/notes/database/NotesSubjectMigration.java
@@ -7,7 +7,7 @@ public class NotesSubjectMigration {
     public static final androidx.room.migration.Migration MIGRATION_1_2 = new androidx.room.migration.Migration(1, 2) {
         @Override
         public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE notesSubject ADD COLUMN sortOrder INT DEFAULT " +
+            database.execSQL("ALTER TABLE notesSubject ADD COLUMN sortOrder INTEGER NOT NULL DEFAULT " +
                     NotesSubject.SORT_ALPHABETICAL_ASC);
         }
     };

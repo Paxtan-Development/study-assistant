e66eda0341edb34d21e65719eed38df3d2d7d70a
PC Chin <chin.pengcheng@gmail.com>
2019-07-07 22:27:07 +0800

    Resolved issue #22

--- app/src/androidTest/java/com/pcchin/studyassistant/AndroidSecurityTest.java
+++ app/src/androidTest/java/com/pcchin/studyassistant/AndroidSecurityTest.java
@@ -5,6 +5,8 @@ import com.pcchin.studyassistant.functions.SecurityFunctions;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /** Test hashing and encryption/decryption functions that require
  * external dependencies such as PBKDF2. **/
 public class AndroidSecurityTest {
@@ -23,6 +25,21 @@ public class AndroidSecurityTest {
         responseArray = SecurityFunctions.subjectEncrypt(AndroidTestFunctions.randomString(2),
                 AndroidTestFunctions.randomString(2), null);
         Assert.assertNotNull(responseArray);
+    }
+
+    /** Check if the encryption & decryption function for the .subject files work. **/
+    @Test
+    public void testSubjectEncryptDecrypt() {
+        // Normal input
+        String testTitle = AndroidTestFunctions.randomString(TEST_COUNT);
+        String testPassword = AndroidTestFunctions.randomString(TEST_COUNT);
+        ArrayList<ArrayList<String>> testContents = AndroidTestFunctions.randomArray(TEST_COUNT);
+        byte[] testOutput = SecurityFunctions.subjectEncrypt(testTitle, testPassword, testContents);
+        Assert.assertNotNull(testOutput);
+
+        ArrayList<ArrayList<String>> testResponse = SecurityFunctions.subjectDecrypt(testTitle,
+                testPassword, testOutput);
+        Assert.assertEquals(testContents, testResponse);
     }
 
     /** Check if the PBKDF2 algorithm is working. **/
